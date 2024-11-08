92fd6c86f0ac12b7351fc73fe14b66073308ed66
Marco Garofalo <marcogarofalo.personal@gmail.com>
2024-11-08 19:52:39 +0000

    Initialise repo

---
+++ downstream/src/main/kotlin/com/example/connect/downstream/DownstreamApplication.kt
@@ -0,0 +1,11 @@
+package com.example.connect.downstream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DownstreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<DownstreamApplication>(*args)
+}
---
+++ downstream/src/test/kotlin/com/example/connect/downstream/DownstreamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.connect.downstream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DownstreamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
